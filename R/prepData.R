cf4ffc2e5da526ca4e6db117b820e899d6867b77
Noah Lorincz-Comi <39975632+noahlorinczcomi@users.noreply.github.com>
2023-03-01 15:04:13 -0500

    Update prepData.R

--- R/prepData.R
+++ R/prepData.R
@@ -27,7 +27,7 @@ prepData=function(betaX,betaY,seX,seY,R,verbose=TRUE) {
   Ryy=R[1:q,1:q]; Rxx=R[(q+1):(p+q),(q+1):(p+q)];Rxy=R[(q+1):(q+p),1:q]
   UU=array(dim=c(p,p,m));UV=array(dim=c(p,q,m));VV=array(dim=c(q,q,m))
   for(i in 1:m) {
-    sx=seX[i,];sy=seY[i,]
+    sx=seX[i,];sy=as.matrix(seY[i,])
     UU[,,i]=diag(sx)%*%Rxx%*%diag(sx)
     VV[,,i]=diag(sy)%*%Ryy%*%diag(sy)
     UV[,,i]=diag(sx)%*%Rxy%*%diag(sy)
