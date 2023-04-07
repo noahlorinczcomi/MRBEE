ed40685d0dfa6547cfe5332db30347965f97c0a3
Noah Lorincz-Comi <39975632+noahlorinczcomi@users.noreply.github.com>
2023-04-07 13:14:09 -0400

    Delete array.to.list.R

--- R/array.to.list.R
+++
@@ -1,9 +0,0 @@
-#' A helping function to convert a 3d array into a list
-#'
-#' @param .array an array of dimensions axbxc to be converted into a list of length c with elements axb matrices
-#' @keywords preparing data
-#' @export 
-#' @examples
-#' array.to.list()
-
-array.to.list=function(.array) lapply(seq(dim(.array)[3]), function(x) .array[ , , x])
