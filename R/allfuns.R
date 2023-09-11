cb43c7a5ee330dd41e98ce5a5e6f92abeecec661
Noah Lorincz-Comi <39975632+noahlorinczcomi@users.noreply.github.com>
2023-09-11 16:56:48 -0400

    Update allfuns.R

--- R/allfuns.R
+++ R/allfuns.R
@@ -1794,13 +1794,7 @@ prepData=function(bT,IVInds=1:nrow(bT$EstHarm),outcome_index=1,verbose=TRUE) {
   }
   prepDataList=list(betaX=betaX,betaY=betaY,UU=UU,UV=UV,VV=VV)
   if(verbose) {
-    mess1=paste0('Assuming that the outcome is in the ',oi,' index position of biasTerms(<>)$EstHarm, biasTerms(<>)$SEHarm, and biasTerms(<>)$R')
-    mess2='You can change this be changing the integer value given to the `oi` argument'
-    print(mess1)
-    print(' ')
-    print(mess2)
-    print(' ')
-    print('you can silence this message by setting the `verbose` argument to FALSE')
+    cat('Assuming that the outcome is in the ',oi,' index position of biasTerms(<>)$EstHarm, biasTerms(<>)$SEHarm, and biasTerms(<>)$R \n You can change this be changing the integer value given to the `oi` argument \n you can silence this message by setting the `verbose` argument to FALSE \n')
   }
   return(prepDataList)
 }
